32c70943c584378170ca584d21e080b37df0f660
Muskan Shroti <shrotimuskan@gmail.com>
2025-03-11 15:29:05 +0530

    Add files via upload

---
+++ ConIN.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javacollege;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ConIN {
+   
+  int x,y;
+    ConIN(int a , int b){
+        this.x =a;
+        this.y = b;//assigning values in x and y for accessing them later
+        System.out.println(a+b);
+    }
+}
