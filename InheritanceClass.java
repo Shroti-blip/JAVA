2ebc2d3dfd07695bb5bfa2dbdd078bb05b118748
Muskan Shroti <shrotimuskan@gmail.com>
2025-03-13 17:09:36 +0530

    Add files via upload

---
+++ InheritanceClass.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javacollege;
+
+/**
+ *
+ * @author ASUS
+ */
+public class InheritanceClass {
+     void method1(int a , int b){
+        System.out.println(a+b);
+    }
+     
+     
+}
