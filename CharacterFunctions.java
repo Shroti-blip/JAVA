f5b5d7e787ac93c1b6dc668f20edb04b5303518c
Muskan Shroti <shrotimuskan@gmail.com>
2025-03-16 13:07:01 +0530

    Add files via upload

---
+++ CharacterFunctions.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javacollege;
+
+/**
+ *
+ * @author ASUS
+ */
+public class CharacterFunctions {
+
+   
+    public static void main(String[] args) {
+        String str = "      Muskan      ";
+        System.out.println(str);
+        System.out.println(str.trim());
+        char ch = str.charAt(1);
+        System.out.println(ch);
+        
+        char ch1 = 't';
+        boolean a = Character.isDigit(ch1);
+        System.out.println(a);
+        
+        boolean b = Character.isWhitespace(ch1);
+        System.out.println(b);
+        
+        boolean c = Character.isLetter(ch1);
+        System.out.println(c);
+        
+        boolean d = Character.isLetterOrDigit(ch1);
+        System.out.println(d);
+        
+        
+    }
+    
+}
