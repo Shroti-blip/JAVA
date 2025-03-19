f74f2e0416bcd7b461e721d7a963c7b84487b05f
Muskan Shroti <shrotimuskan@gmail.com>
2025-03-19 22:25:12 +0530

    Add files via upload

---
+++ Interface/Interface1.java
@@ -0,0 +1,19 @@
+package javacollege;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public interface Interface1 {
+    void method1();
+    void method2();
+    void method3();
+//    void method4(){ interface abstract method can not have body
+//        System.out.println("Hello World");
+//    }
+}
