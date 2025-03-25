5640f466513d545c87dec309c7f218d2e504e80d
Muskan Shroti <shrotimuskan@gmail.com>
2025-03-25 14:33:52 +0530

    Add files via upload

---
+++ Inheritace/CricketIN.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javacollege;
+
+/**
+ *
+ * @author ASUS
+ */
+public class CricketIN extends SportsIN{
+String player;
+    void setInfo(String player){
+        this.player = player;
+    }
+    void getInfo(){
+        System.out.println(player);
+    }
+    public static void main(String[] args) {
+        CricketIN ci = new CricketIN();//create object of child class class so we can access both class methods
+        ci.setInfo("Muskan");
+        ci.getInfo();
+        ci.setData("Cricket", 11);
+        ci.getData();
+    }
+    
+}
